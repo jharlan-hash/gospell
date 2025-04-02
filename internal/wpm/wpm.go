c77d1bf8dd825560b524fb9bc73683189a84e1ea
Lazyhacker <lazyhacker@users.noreply.github.com>
2025-04-02 01:27:38 -0700

    Update wpm.go

--- internal/wpm/wpm.go
+++ internal/wpm/wpm.go
@@ -5,15 +5,13 @@ import (
 	"unicode/utf8"
 )
 
-var wpm int
-
 // CalculateWpm calculates the words per minute (WPM) based on the given word and time interval.
 // It takes a word (string), the initial time (time.Time), and the final time (time.Time) as input.
 // It returns the calculated WPM as an integer.
 func CalculateWpm(word string, initialTime time.Time, finalTime time.Time) int {
 	// If the word is empty, return the last calculated wpm
 	if word == "" {
-		return wpm
+		return 0
 	}
 
 	timeElapsed := finalTime.Sub(initialTime)
@@ -26,6 +24,5 @@ func CalculateWpm(word string, initialTime time.Time, finalTime time.Time) int {
 
 	numWords := float64(utf8.RuneCountInString(word)) / 5.0
 
-	wpm = int(numWords / elapsedMinutes)
-	return wpm
+	return int(numWords / elapsedMinutes)
 }
