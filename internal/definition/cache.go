6c0af799c93eeb4b574651f82e9df5541a22eb02
Lazyhacker <lazyhacker@users.noreply.github.com>
2025-04-02 01:17:34 -0700

    Update cache.go
    Let the caller know and handle the error.  What was there just print the error to stdout but the caller had no idea something went wrong during decoding.

--- internal/definition/cache.go
+++ internal/definition/cache.go
@@ -12,18 +12,16 @@ var fs embed.FS
 
 // LoadCache loads the wordmap cache from a file.
 // It returns a Dictionary, which is a map of words to an Entry slice.
-func LoadCache() Dictionary {
+func LoadCache() (Dictionary, err) {
 	f, err := fs.Open("wordmap.gob")
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Unable to open wordmap. %v", err)
 	}
 
-	enc := gob.NewDecoder(f)
 	cache := make(Dictionary)
-
-	if err := enc.Decode(&cache); err != nil {
-		fmt.Println("Error decoding cache:", err)
+	if err := gob.NewDecoder(f).Decode(&cache); err != nil {
+		return nil, fmt.Errorf("Error decoding cache.  %v", err)
 	}
 
-	return cache
+	return cache, nil
 }
